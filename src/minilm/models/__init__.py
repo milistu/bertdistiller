9d11f8ae8d588e3468dd6609abc58e4a29869274
milistu <studen.m98@gmail.com>
2025-02-25 09:13:08 +0100

    Add init.

--- src/minilm/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/minilm/models/__init__.py
-
-__version__ = "0.1.0"
-
-from .minilm import MiniLM
-
-__all__ = ["MiniLM"]
--- src/minilm/trainer/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/minilm/trainer/__init__.py
-
-__version__ = "0.1.0"
-
-from .distillation import DistillationConfig, DistillationTrainer
-
-__all__ = ["DistillationConfig", "DistillationTrainer"]
